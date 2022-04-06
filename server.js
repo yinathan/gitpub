add9a7209c3926131765de42895ac7792e30d9bc
Nathan Yi <nathanyi112@gmail.com>
2022-04-06 00:24:25 -0500

    set up /drinks route

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const drinks = require("./models/drinks.js")
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
+
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
+
 
 app.listen(PORT, () => {
     console.log("working")
